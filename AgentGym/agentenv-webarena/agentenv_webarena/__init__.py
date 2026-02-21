936119f882da41715ce152bb72db16fc61f421e4
lchen-T <1969773923@qq.com>
2026-02-21 23:28:16 +0800

    Update AgentGym/agentenv-webarena/agentenv_webarena/__init__.py

--- AgentGym/agentenv-webarena/agentenv_webarena/__init__.py
+++ AgentGym/agentenv-webarena/agentenv_webarena/__init__.py
@@ -5,21 +5,20 @@ sys.path.append(
     os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "webarena")
 )
 
-# webarena urls
-os.environ["SHOPPING"] = "http://metis.lti.cs.cmu.edu:7770"
-os.environ["SHOPPING_ADMIN"] = (
-    "http://metis.lti.cs.cmu.edu:7780/admin"
+# webarena urls (allow external override via env)
+os.environ.setdefault("SHOPPING", "http://127.0.0.1:7770")
+os.environ.setdefault("SHOPPING_ADMIN", "http://127.0.0.1:7780/admin")
+os.environ.setdefault("REDDIT", "http://127.0.0.1:9999")
+os.environ.setdefault("GITLAB", "http://127.0.0.1:8023")
+os.environ.setdefault("MAP", "http://127.0.0.1:3000")
+os.environ.setdefault(
+    "WIKIPEDIA",
+    "http://127.0.0.1:8888/wikipedia_en_all_maxi_2022-05/A/User:The_other_Kiwix_guy/Landing",
 )
-os.environ["REDDIT"] = "http://metis.lti.cs.cmu.edu:9999"
-os.environ["GITLAB"] = "http://metis.lti.cs.cmu.edu:8023"
-os.environ["MAP"] = "http://metis.lti.cs.cmu.edu:3000"
-os.environ["WIKIPEDIA"] = (
-    "http://metis.lti.cs.cmu.edu:8888/wikipedia_en_all_maxi_2022-05/A/User:The_other_Kiwix_guy/Landing"
-)
-os.environ["HOMEPAGE"] = "http://metis.lti.cs.cmu.edu:4399"
+os.environ.setdefault("HOMEPAGE", "http://127.0.0.1:4399")
 
-os.environ["OPENAI_API_KEY"] = ""
-os.environ["OPENAI_BASE_URL"] = ""
+os.environ.setdefault("OPENAI_API_KEY", "")
+os.environ.setdefault("OPENAI_BASE_URL", "")
 
 os.chdir("./webarena")
 
