eb8698b4ab9643378e12e43bc9b4a91b94a5f018
lchen-T <1969773923@qq.com>
2026-02-21 23:28:19 +0800

    Update scripts/env_server/start_alfworld.sh

--- scripts/env_server/start_alfworld.sh
+++ scripts/env_server/start_alfworld.sh
@@ -1,2 +1,4 @@
-source ../uv_alfworld/bin/activate
+source "$HOME/uv_envs/uv_alfworld/bin/activate"
+REPO_ROOT="${REPO_ROOT:-$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)}"
+export ALFWORLD_DATA="${ALFWORLD_DATA:-$REPO_ROOT/.cache/alfworld}"
 alfworld --host 0.0.0.0 --port 36001
